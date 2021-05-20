6315a7c71f6714a57821c48d2d962e852512b5a4
Softwareengineering-S <79382145+Softwareengineering-S@users.noreply.github.com>
2021-05-20 09:43:23 +0200

    added files via upload

---
+++ SwiftUIByExample/SwiftUIByExample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIByExample
+//
+//  Created by Monique Shaqiri on 18.05.21.
+//  Copyright © 2021 Monique Shaqiri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
